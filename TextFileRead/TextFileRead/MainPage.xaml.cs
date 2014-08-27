75e315a87c72127fdc1d69bd6ce3b19b81b1f953
Balasubramani <balageetha@live.com>
2014-08-27 14:10:41 +0530

    Read Text from TextFile using StreamReader Class
    Method used to read text from text file using StreamReader class in Windows Phone.

---
+++ TextFileRead/TextFileRead/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Phone.Controls;
+using System;
+using System.IO;
+using System.Windows;
+
+namespace TextFileRead
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+            textBlock.Text = ReadFile(@"/TextFileRead;component/TextFile.txt");
+        }
+
+        private string ReadFile(string fileName) // fileName is the name of the text file, you would like to read
+        {
+            var resourceStream = Application.GetResourceStream(new Uri(fileName, UriKind.Relative)); // Get resource stream from resource file
+            if (resourceStream != null)
+            {
+                Stream myStream = resourceStream.Stream; 
+                if (myStream.CanRead)
+                {
+                    StreamReader streamReader = new StreamReader(myStream); // Reading data using StreamReader
+                    return streamReader.ReadToEnd(); // Reading upto the end of the file using ReadToEnd() function.
+                }
+            }
+
+            return "Null";
+        }
+    }
+}
